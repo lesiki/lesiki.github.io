761c85fe5884c489b036c98c85f6f6b08d27a5e2
Sitati Kituyi <sitati.kituyi@googlemail.com>
2014-02-09 16:52:43 +0300

    Animate display of coloured border

--- sitati.js
+++ sitati.js
@@ -1,1 +1,16 @@
 $('.ui.dropdown').dropdown();
+var homepage = function() {
+	var
+	fadeInFauxBorderByIndex = function(index) {
+		setTimeout(function() {
+			$('.fauxBorder .border').eq(index).addClass('displayed');
+			if($('.fauxBorder .border').eq(index)) {
+				fadeInFauxBorderByIndex(index + 1);
+			}
+		}, 200);
+	},
+	fadeInFauxBorders = function() {
+		fadeInFauxBorderByIndex(0);	
+	};
+	fadeInFauxBorders();
+}();
